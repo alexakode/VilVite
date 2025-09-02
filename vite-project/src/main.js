19a21c4b97705077099352a689ac5bb9b6823a46
Alexander <alexanderm.kodehode@gmail.com>
2025-09-02 10:45:54 +0200

    trying to follow lesson

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -14,11 +14,12 @@ document.querySelector('#app').innerHTML = `
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
+      <button id="generate-uuid" type="button">Generate UUID</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
     </p>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
+generateUUIDButton(document.querySelector("#app"));
+setupCounter(document.querySelector("#counter"));
