b62a039d635c637831c36383905d43ca0a009913
Alexander <alexanderm.kodehode@gmail.com>
2025-09-02 09:56:19 +0200

    npm packages

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import { choice, randInt } from "pretty-random";
 
-console.log(randInt(1, 10));
+console.log(randInt(1, 10()));
+
+// install packages with npm i
+// e.g. pretty-random
+// check github dependabot
