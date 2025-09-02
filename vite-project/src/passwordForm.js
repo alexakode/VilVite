d863f87e05e68e5e9950740219d19cc03794db4b
Alexander <alexanderm.kodehode@gmail.com>
2025-09-02 11:13:37 +0200

    work on password form

--- vite-project/src/passwordForm.js
+++ vite-project/src/passwordForm.js
@@ -2,7 +2,10 @@ const passwordForm = (formElement) => {
   formElement.addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(form);
+    const email = formData.get("email");
+    const password = formData.get("password");
     const hashedPassword = await hashPassword(formData.get("password"));
-    alert(`Hashed Password: ${hashedPassword}`);
+    alert(`Email: ${email}\n Hashed Password: ${hashedPassword}`);
+    console.log(`Email: ${email}\n Hashed Password: ${hashedPassword}`);
   });
 };
