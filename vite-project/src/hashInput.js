838986cbc6a79f3055fe0ed7f6e78c94ad41ce0f
Alexander <alexanderm.kodehode@gmail.com>
2025-09-02 11:37:56 +0200

    hash passwords

--- vite-project/src/hashInput.js
+++ vite-project/src/hashInput.js
@@ -1,4 +1,6 @@
-const hashInput = (input) => {
-  const hashed = btoa(input);
-  return hashed;
+import bcrypt, { hash } from "bcryptjs";
+export const hashInput = async (input) => {
+  console.log(input);
+  const hashedPassword = await bcrypt.hash(input, 10);
+  return hashedPassword;
 };
